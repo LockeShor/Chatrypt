84eca7afd4fd1cbe6085f0274acf05da9c64cbbf
Redpug111 <75901583+Redpug111@users.noreply.github.com>
2023-03-10 17:16:08 -0500

    Fix Truncation
    yippee

--- decrypt.js
+++ decrypt.js
@@ -2,16 +2,14 @@ const fs = require('fs')
 const crypto = require('node:crypto')
 
 data_in = JSON.parse(fs.readFileSync("./io.txt").toString())
-//console.log(data_in)
 for (i = 1;i< 4;i++){
     pub = fs.readFileSync(`./test_data/RSApub${i}.txt`)
     sec = fs.readFileSync(`./test_data/RSAsec${i}.txt`)
     encrypted_key = data_in[pub.toString()]
-    //console.log(Buffer.from(encrypted_key))
     decrypted = crypto.privateDecrypt(crypto.createPrivateKey(sec), Buffer.from(encrypted_key))
     console.log(`${i} decrypted key:\t`, decrypted)
     IV = data_in["IV"]
     msg = data_in["message"]
     decipher = crypto.createDecipheriv("aes-256-cbc", decrypted, Buffer.from(IV));
-    console.log(decipher.update(msg, "hex", "utf-8"))
+    console.log(decipher.update(msg, "hex", "utf-8") + decipher.final("utf-8"))
 }
--- encrypt.js
+++ encrypt.js
@@ -7,23 +7,17 @@ aes_key = crypto.randomBytes(32)
 console.log("unencrypted key:\t", aes_key)
 
 out = {}
-message = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
-//BUG ^^ Truncates to multiple of 16 bytes, fix by adding 16 extra bytes to add to a multiple of 16???
-//OHHH I NEED TO ADD PADDING
+message = "Hello World!"
+
 const cipher = crypto.createCipheriv("aes-256-cbc", aes_key, IV)
-//TODO: encrypt IV?
+
 out["IV"] = IV
-out["message"] = cipher.update(message, "utf-8", "hex")
+out["message"] = cipher.update(message, "utf-8", "hex") + cipher.final("hex")
 
 for (i = 1;i< 4;i++){
     pub = fs.readFileSync(`./test_data/RSApub${i}.txt`)
-    //sec = fs.readFileSync(`./test_data/RSAsec${i}.txt`)
     encrypted_key = crypto.publicEncrypt(crypto.createPublicKey(pub), aes_key)
-    //decrypted = crypto.privateDecrypt(crypto.createPrivateKey(sec), encrypted_key)
-    //console.log(`${i} decrypted key:\t`, decrypted)
     //console.log(encrypted_key)
     out[pub.toString()] = encrypted_key
-    //console.log(encrypted_key.toString())
 }
-//console.log(out)
 fs.writeFileSync("./io.txt", JSON.stringify(out, null, 2))
